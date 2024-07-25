2602f5bfdabe681f421c88745df3266081daa74e
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-25 16:31:34 +0200

    Update barlow_twins_datamodule.py

--- ORDNA/data/barlow_twins_datamodule.py
+++ ORDNA/data/barlow_twins_datamodule.py
@@ -1,33 +1,25 @@
-class BarlowTwinsDataModule(pl.LightningDataModule):
-    def __init__(self, samples_dir: Path, labels_file: Path, habitats_file: Path, sequence_length: int, sample_subset_size: int, batch_size: int = 8) -> None:
-        super().__init__()
+import torch
+import os
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+from typing import Optional
+from pathlib import Path
+from ORDNA.data.barlow_twins_dataset import BarlowTwinsDataset
 
-        self.train_samples_dir = samples_dir / "train"
-        self.val_samples_dir = samples_dir / "valid"
+class BarlowTwinsDataModule(pl.LightningDataModule):
+    def __init__(self, embeddings_file: Path, labels_file: Path, habitats_file: Path, batch_size: int = 8) -> None:
+        super().__init__()
+        self.embeddings_file = embeddings_file
         self.labels_file = labels_file
         self.habitats_file = habitats_file
-        self.sequence_length = sequence_length
-        self.sample_subset_size = sample_subset_size
         self.batch_size = batch_size
-
-        assert(batch_size is not None) 
 
     def setup(self, stage: Optional[str] = None) -> None:
         if stage == 'fit' or stage is None:
             self.train_dataset = BarlowTwinsDataset(
-                samples_dir=self.train_samples_dir,
+                embeddings_file=self.embeddings_file,
                 labels_file=self.labels_file,
-                habitats_file=self.habitats_file,
-                sample_subset_size=self.sample_subset_size,
-                sequence_length=self.sequence_length
-            )
-
-            self.val_dataset = BarlowTwinsDataset(
-                samples_dir=self.val_samples_dir,
-                labels_file=self.labels_file,
-                habitats_file=self.habitats_file,
-                sample_subset_size=self.sample_subset_size,
-                sequence_length=self.sequence_length
+                habitats_file=self.habitats_file
             )
 
     def train_dataloader(self) -> DataLoader:
@@ -42,7 +34,7 @@ class BarlowTwinsDataModule(pl.LightningDataModule):
 
     def val_dataloader(self) -> DataLoader:
         return DataLoader(
-            dataset=self.val_dataset, 
+            dataset=self.train_dataset, 
             batch_size=self.batch_size, 
             shuffle=False, 
             num_workers=12, 
