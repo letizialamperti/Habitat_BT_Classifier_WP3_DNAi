697ca2479f15c2817fdcace67e2a6706235ddd25
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-26 09:12:16 +0200

    Update batch_script_corse_classifier.sh

--- batch_script_corse_classifier.sh
+++ batch_script_corse_classifier.sh
@@ -35,6 +35,6 @@ srun -ul $HOME/miniconda3/envs/diaus_1/bin/python training_Classifier.py \
     --num_classes 4 \
     --batch_size 10 \
     --initial_learning_rate 1e-3 \
-    --max_epochs 10
+    --max_epochs 30
 
 echo "Training completed successfully."
