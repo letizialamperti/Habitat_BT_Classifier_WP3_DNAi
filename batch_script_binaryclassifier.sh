f460d442a138e324645e66c3545859db38c6850c
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-26 16:18:32 +0200

    Update batch_script_binaryclassifier.sh

--- batch_script_binaryclassifier.sh
+++ batch_script_binaryclassifier.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 # SLURM SBATCH Directives
-#SBATCH --job-name=classifier-job
+#SBATCH --job-name=bin_classifier-job
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -19,17 +19,18 @@ module load cray-python
 source activate diaus_1
 
 # Define dataset and labels path
-DATASET_DIR="/store/sdsc/sd29/letizia/sud_corse"
-LABELS_FILE="label/labels_numeric_binary_MPA_Sud_corse.csv"
+EMBEDDINGS_FILE="/scratch/snx3000/llampert/embedding_coords/new_embedding_coordinates_sud_corse__.csv"
+PROTECTION_FILE="/store/sdsc/sd29/letizia/label/labels_numeric_binary_MPA_Sud_corse.csv"
+HABITAT_FILE="/store/sdsc/sd29/letizia/habitat/labels_habitat_sud_corse.csv"
 
 # Command to run the Python script
 echo "Starting the training process."
 export CUDA_LAUNCH_BLOCKING=1
-srun -ul $HOME/miniconda3/envs/diaus_1/bin/python training_binaryclassifier.py \
+srun -ul $HOME/miniconda3/envs/diaus_1/bin/python training_binary_classifier.py \
     --arg_log True \
-    --samples_dir $DATASET_DIR \
-    --labels_file $LABELS_FILE \
-    --embedder_type barlow_twins \
+    --embeddings_file $EMBEDDINGS_FILE \
+    --protection_file $PROTECTION_FILE \
+    --habitat_file $HABITAT_FILE \
     --sequence_length 300 \
     --sample_subset_size 500 \
     --num_classes 2 \
@@ -38,7 +39,7 @@ srun -ul $HOME/miniconda3/envs/diaus_1/bin/python training_binaryclassifier.py \
     --sample_repr_dim 128 \
     --sample_emb_dim 64 \
     --initial_learning_rate 1e-3 \
-    --max_epochs 2
+    --max_epochs 2 \
+    --accelerator gpu
 
 echo "Training completed successfully."
-
